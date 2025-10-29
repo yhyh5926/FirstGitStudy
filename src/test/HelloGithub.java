30bd4fff8f6f981102d202be84353169e241e52d
yhyh5926 <yhyh5926@naver.com>
2025-10-29 10:23:11 +0900

    commit

--- src/test/HelloGithub.java
+++ src/test/HelloGithub.java
@@ -4,9 +4,10 @@ public class HelloGithub {
 
 	public static void main(String[] args) {
 		System.out.println("1.프로젝트생성");
-		System.out.println("hello java");
+		System.out.println("2.내용수정1");
 		System.out.println("3.집에서 작업");
-
+		System.out.println("4.학원-추가 작업");
+		System.out.println("4-1.까먹고 작업");
 	}
 
 }
